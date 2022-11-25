27ea80d63df4058e38321e3d32a3511274751863
chela-droid <[200~chelangatcaroline70@gmail.com~>
2022-11-25 12:55:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution in this file!
+// function customerName(){
+//     let name = bob 
+// }
+// function customerName(){
+//  var customerName = bob;
+//  return customerName(bob);
+// }
+
+// const customerName = bob 
+// function customerName(){
+//     return customerName (bob)
+// }
+// function upperCaseCustomerName(){
+//     window.upperCaseCustomerName (bob)
+//     return toUpperCase()
+// }
+
+ var customerName= 'bob';
+
+function customerName (bob){
+    return (customerName.toUpperCase);
+    
+}
+function upperCaseCustomerName(bob){
+return customerName = customerName.toUpperCase()
+}
+function setBestCustomer(){
+    return (bestCustomer = 'not bob')
+}
+function overwriteBestCustomer(){
+    return (bestCustomer= 'maybe bob')
+}
+
+const leastFavorite = 'bob'
+function changeLeastFavoriteCustomer(){
+    leastFavorite = 'not bob'
+    return (changeLeastFavoriteCustomer);
+}
